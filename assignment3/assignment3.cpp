f7352705f8a64c381239573bbfbfc3666fcaff6d
GeorgeHutchinson1627 <82395601+GeorgeHutchinson1627@users.noreply.github.com>
2021-04-19 17:44:31 -0700

    Add files via upload

---
+++ assignment3/assignment3.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+//Link to website I looked at to better understand the arrow operator.
+//https://www.geeksforgeeks.org/arrow-operator-in-c-c-with-examples/
+
+
+
+
+
+struct node{
+    int data;
+    node *next_node;
+};
+node *front=nullptr;
+node *back=nullptr;
+
+void enqueue(int value)
+{
+    node *new_node= new node();
+    new_node->data=value;
+    new_node->next_node=nullptr;
+    
+    if (front==nullptr)
+    {
+    front=new_node;
+    back=new_node;
+    }
+    else
+    {
+    back->next_node=new_node;
+    back=new_node;
+
+    }
+}
+
+int dequeue(){
+    int removed_data;
+    node* new_node= front;
+    removed_data=front->data;
+    front=front->next_node;
+    return removed_data;
+}
+
+
+void peek(){
+    int front_value;
+    front_value=front->data;
+    cout << front_value<<"\n";
+}
+
+int main()
+{
+enqueue(2);
+enqueue(3);
+enqueue(5);
+enqueue(6);
+peek();
+cout<< dequeue()<<"\n";
+cout <<dequeue()<<"\n";
+cout<< dequeue()<<"\n";
+
+return 0;
+}
