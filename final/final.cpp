94d87731cd6cf810b53a484c05a581711f055b9a
GeorgeHutchinson1627 <82395601+GeorgeHutchinson1627@users.noreply.github.com>
2021-06-11 18:11:17 -0700

    Update final.cpp

--- final/final.cpp
+++ final/final.cpp
@@ -1,15 +1,20 @@
+
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
  
-//nodes only given the possibility of two neighbors
+
+//I looked into vectors more to improve my code
+
+//nodes struct
 struct node{
     int data;
-    node*next_node;
-    node*second_node;
-    int weight=9999;
-    int second_weight=9999;
-    node*parent;
+    vector<node*> adjecents;
+    vector<int> weights;
+    node* parent;
+    int value=9999;
+    bool visited;
 };
 
 //creats the vertices of the graph
@@ -18,7 +23,6 @@ node** graph_maxtrix(int number_nodes){
     for(int i = 0; i < number_nodes; ++i) {
     array_pointers[i] = new node;
     array_pointers[i]->data=i;
-    array_pointers[i]->next_node=nullptr;
     }
     return array_pointers;
 }
@@ -26,39 +30,64 @@ node** graph_maxtrix(int number_nodes){
 //creats the edges for a simple graph
 void add_edge(int start,int finish,int weight,node** graph){
     node**array_pointer=graph;
-    
     node* end_node=array_pointer[finish];
-    if (array_pointer[start]->next_node==nullptr){
-        array_pointer[start]->next_node=end_node;
-        array_pointer[start]->weight=weight;
-    }
-    else{
-        array_pointer[start]->second_node=end_node;
-        array_pointer[start]->second_weight=weight;
-    }
-    
+    array_pointer[start]->adjecents.push_back(end_node);
+    array_pointer[start]->weights.push_back(weight);
 }
 //very simple minimum spanning tree
 void MST(node**graph,int number_nodes){
-    node**array_pointer=graph;
-    for(int i=0;i<number_nodes;i++){
-        int first_value=array_pointer[i]->weight;
-        int second_value=array_pointer[i]->second_weight;
-        
-        if (first_value<second_value){
-            cout<<array_pointer[i]->data<<" --> "<<array_pointer[i]->next_node->data<<"\n";
-        }
-        else if(first_value>second_value){
-            cout<<array_pointer[i]->data<<" --> "<<array_pointer[i]->second_node->data<<"\n";
-        }
+    vector<int> visited;
+    int current_number=0;
+    int min=9999;
+    cout<<"--MST--"<<"\n";
+     for(int i=0;i<number_nodes;i++){
+        min = *min_element(graph[current_number]->weights.begin(), graph[current_number]->weights.end());
+         for(int j=0;j<graph[current_number]->adjecents.size();j++){
+             if (find(visited.begin(), visited.end(), graph[current_number]->adjecents[j]->data) != visited.end()==false and 
+             (graph[current_number]->weights[j]==min)){
+                 visited.push_back(current_number);
+                 cout<<current_number<<" --> "<<graph[current_number]->adjecents[j]->data<<"\n";
+                 current_number=graph[current_number]->adjecents[j]->data;
+                 
+                
+                 
+             }
+         }
     }
 }
 
-//simple shortest path function
+
+//simple shortest path function it prints the shortest path in reverse.
 void shortest_path(int start,int finish,node** graph){
-    node**array_pointer=graph;
-    
-    
+    vector<node*> queue;
+    node* current_adjecent;
+    graph[0]->value=0;
+    queue.push_back(graph[start]);
+    graph[start]->parent=graph[start];
+    while(queue.empty()==false){
+        for(int j=0;j<queue[0]->adjecents.size();j++){
+            current_adjecent=queue[0]->adjecents[j];
+            if (current_adjecent->visited==false and (current_adjecent->value>queue[0]->value+queue[0]->weights[j])){
+                current_adjecent->visited=true;
+                current_adjecent->value=queue[0]->value+queue[0]->weights[j];
+                current_adjecent->parent=queue[0];
+                queue.push_back(current_adjecent);
+                
+            }
+        }
+        queue.erase(queue.begin());
+        }
+    node* current_node=graph[finish];
+    vector<node*>queue2;
+    queue2.push_back(graph[finish]);
+    cout<<"Shortest path"<<"\n";
+    while (current_node->parent!=current_node){
+        cout<<current_node->data<<" <-- ";
+        current_node=current_node->parent;
+        cout<<current_node->data<<"\n";
+        queue2.push_back(current_node);
+
+    }
 }
 
 int main(){
@@ -66,13 +95,13 @@ int main(){
     //The shortest path is also probably O(n).
     node** graph=graph_maxtrix(6);
     add_edge(0,2,2,graph);
-    add_edge(0,3,5,graph);
-    add_edge(1,2,9,graph);
-    add_edge(1,3,3,graph);
+    add_edge(0,1,4,graph);
+    add_edge(2,5,3,graph);
     add_edge(2,4,6,graph);
-    add_edge(2,5,7,graph);
+    add_edge(4,1,3,graph);
+    add_edge(5,4,3,graph);
     
-    cout<<graph[0]->next_node->data<<"\n";
-    cout<<graph[0]->second_node->data<<"\n";
+    shortest_path(0,5,graph);
     MST(graph,6);
+    
 }
